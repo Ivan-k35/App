08d3e9fda9a216d8c8f17678e9f2fe24429a3b37
Ivan-k35 <ivan.koshkin35@gmail.com>
2021-11-09 15:54:34 +0300

    Изменил файлы base.html, models.py, urls.py

--- taskmaneger/main/urls.py
+++ taskmaneger/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about),
-    path('support', views.support)
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('support', views.support, name='support')
 ]
